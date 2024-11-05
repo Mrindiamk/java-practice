5fae20e266f3056e8cd335bdaa69b42d38e2c8ad
YamahaNmax <k4kenishalearning@gmail.com>
2024-11-05 19:19:42 +0000

    Push

---
+++ java p/src/first/MySecond.java
@@ -0,0 +1,14 @@
+package first;
+
+public class MySecond {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");}
+
+}
---
+++ java p/src/variable/LocalVariable.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class LocalVariable {
+
+    public static void main(String[] args) {
+
+       String  detail= "kenisha patel";
+       int x= 7;
+        System.out.println(detail);
+        System.out.println(x);
+
+    }
+}
